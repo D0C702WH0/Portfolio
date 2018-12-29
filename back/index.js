f14e2bd50e22b9268d398c779ea8389dac330c58
D0C702WH0 <jemyplu@gmail.com>
2018-12-29 21:07:15 +0100

    server init

---
+++ back/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 5000;
+const models = require("./models");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+
+models.sequelize
+  .sync()
+  .then(() =>
+    app.listen(port, console.log(`server listening on port ${port}`))
+  );
---
+++ back/migrations/20181229190955-create-photo.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('photos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      idCategory: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: `categories`,
+          key: `id`
+        }
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      path: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('photos');
+  }
+};
