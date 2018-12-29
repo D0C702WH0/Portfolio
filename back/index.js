2c8a5608ff69c9b5e942e929019c718c76e702be
D0C702WH0 <jemyplu@gmail.com>
2018-12-29 22:03:41 +0100

    back office login route

--- back/index.js
+++ back/index.js
@@ -5,11 +5,15 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 const models = require("./models");
+const routerAuth = require("./routes/auth");
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
+
+app.use("/auth", routerAuth);
+
 
 
 models.sequelize
