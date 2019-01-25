2403b88cc1d98f0407d18fc3acce3458d7cdc552
D0C702WH0 <jemyplu@gmail.com>
2019-01-25 08:59:59 +0100

    export upload

--- back/routes/photos.js
+++ back/routes/photos.js
@@ -3,55 +3,56 @@ const Sequelize = require("sequelize");
 const op = Sequelize.Op;
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const aws = require("aws-sdk");
-const multer = require("multer");
-const multerS3 = require("multer-s3");
+// const aws = require("aws-sdk");
+// const multer = require("multer");
+// const multerS3 = require("multer-s3");
 const router = express.Router();
 const models = require("../models");
 const getToken = require("../helpers/getToken");
-const getFileExtension = require("../helpers/getFileExtension");
-const s3Password = process.env.AWS_KEY;
-const s3Id = process.env.AWS_ID;
+const upload = require("../helpers/upload");
+// const getFileExtension = require("../helpers/getFileExtension");
+// const s3Password = process.env.AWS_KEY;
+// const s3Id = process.env.AWS_ID;
 const jwtSecret = process.env.JWT_SECRET;
 
-aws.config.update({
-  secretAccessKey: s3Password,
-  accessKeyId: s3Id
-});
+// aws.config.update({
+//   secretAccessKey: s3Password,
+//   accessKeyId: s3Id
+// });
 
-const s3 = new aws.S3();
+// const s3 = new aws.S3();
 
-const storage = multerS3({
-  s3: s3,
-  acl: "public-read",
-  bucket: "sideprojectbp",
-  metadata: function(req, file, cb) {
-    cb(null, { fieldName: file.fieldname });
-  },
-  key: function(req, file, cb) {
-    cb(null, `${Date.now()}.${getFileExtension(file.mimetype)}`);
-  }
-});
+// const storage = multerS3({
+//   s3: s3,
+//   acl: "public-read",
+//   bucket: "sideprojectbp",
+//   metadata: function(req, file, cb) {
+//     cb(null, { fieldName: file.fieldname });
+//   },
+//   key: function(req, file, cb) {
+//     cb(null, `${Date.now()}.${getFileExtension(file.mimetype)}`);
+//   }
+// });
 
-const upload = multer({
-  dest: "tmp/",
-  storage,
-  fileFilter: (req, file, cb) => {
-    if (
-      !(
-        file.mimetype.includes("image/png") ||
-        file.mimetype.includes("image/jpg") ||
-        file.mimetype.includes("image/jpeg")
-      )
-    ) {
-      cb(new Error("Mauvais format de fichier"));
-    }
-    cb(null, true);
-  },
-  limits: {
-    fileSize: 5 * 1024 * 1024
-  }
-});
+// const upload = multer({
+//   dest: "tmp/",
+//   storage,
+//   fileFilter: (req, file, cb) => {
+//     if (
+//       !(
+//         file.mimetype.includes("image/png") ||
+//         file.mimetype.includes("image/jpg") ||
+//         file.mimetype.includes("image/jpeg")
+//       )
+//     ) {
+//       cb(new Error("Mauvais format de fichier"));
+//     }
+//     cb(null, true);
+//   },
+//   limits: {
+//     fileSize: 5 * 1024 * 1024
+//   }
+// });
 
 router
   .route("/")
